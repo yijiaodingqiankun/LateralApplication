62cb842936a101e23ecff096dfcc73dfa27d9069
zhang <1524832680@qq.com>
2018-04-15 13:13:23 +0800

    侧滑菜单改进成了NavigationView 13:11 zhanghuirong 第3次提交

--- app/src/main/java/com/jiyun/lateralapplication/MainActivity.java
+++ app/src/main/java/com/jiyun/lateralapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jiyun.lateralapplication;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -12,12 +14,16 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private Toolbar toolbar;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (NavigationView) findViewById(R.id.nav);
+
+
         setSupportActionBar(toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBar actionBar = getSupportActionBar();
@@ -28,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
 //            设置一个导航按钮图标
             actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
         }
+
+        navigationView.setCheckedItem(R.id.item1);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
 
     }
 
