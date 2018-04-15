1ecd6930ff269399ce0d3bdf21a37e1e36959e61
zhang <1524832680@qq.com>
2018-04-15 12:21:52 +0800

    给toolbar添加了一个按钮，给侧滑菜单添加了一个按钮，一点按钮，侧滑菜单就会出来 12:21 zhanghuirong 第二次提交

--- app/src/main/java/com/jiyun/lateralapplication/MainActivity.java
+++ app/src/main/java/com/jiyun/lateralapplication/MainActivity.java
@@ -1,14 +1,45 @@
 package com.jiyun.lateralapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+//            让导航按钮显示出来
+            actionBar.setDisplayHomeAsUpEnabled(true);
 
+//            设置一个导航按钮图标
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //将滑动菜单展示出来
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+
+        }
+        return true;
     }
 }
